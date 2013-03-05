3fda925b161998f0b1b5f74ba6c025ddcd834da2
Gaël Chamoulaud <gael@redhat.com>
2013-03-05 22:08:24 +0100

    Fixed some issues with the vim-powerline submodule

--- apply.sh
+++ apply.sh
@@ -11,23 +11,23 @@ cp -r dotvim/* ~/.vim
 # command-T
 vim --version | grep -q '\+ruby'
 if [ $? -eq 0 ];then
-  ruby --version | egrep -q '1\.8\.7|1.9'
-  if [ $? -eq 0 ];then
-    cd ~/.vim/bundle/command-t/ruby/command-t 1>/dev/null
-    rm -f *.o *.so
-    ruby extconf.rb
-    make
-    cd - 1>/dev/null
-  else
-    rm -rf ~/.vim/bundle/command-t
-  fi
+    ruby --version | egrep -q '1\.8\.7|1.9'
+    if [ $? -eq 0 ];then
+        cd ~/.vim/bundle/command-t/ruby/command-t 1>/dev/null
+        rm -f *.o *.so
+        ruby extconf.rb
+        make
+        cd - 1>/dev/null
+    else
+        rm -rf ~/.vim/bundle/command-t
+    fi
 else
-  rm -rf ~/.vim/bundle/command-t
+    rm -rf ~/.vim/bundle/command-t
 fi
 
 # Pyflakes, Flake8 and Jedi only for Python 2.5+
 python -V 2>&1 | grep -q '2\.4'
 if [ $? -eq 0 ];then
-  rm -rf ~/.vim/bundle/flake8
-  rm -rf ~/.vim/bundle/jedi-vim
+    rm -rf ~/.vim/bundle/flake8
+    rm -rf ~/.vim/bundle/jedi-vim
 fi
