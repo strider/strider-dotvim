979ba5e75f666eb4d9bcd29c63699dc8bab36a52
Gaël Chamoulaud <gael@redhat.com>
2014-01-07 15:11:47 +0100

    No more need to compile as I deleted commant-t from my configuration

--- apply.sh
+++ apply.sh
@@ -8,23 +8,6 @@ rm -Rf ~/.vim
 mkdir -p ~/.vim
 cp -r dotvim/* ~/.vim
 
-# command-T
-vim --version | grep -q '\+ruby'
-if [ $? -eq 0 ];then
-    ruby --version | egrep -q '1\.8\.7|1.9|2.0'
-    if [ $? -eq 0 ];then
-        cd ~/.vim/bundle/command-t/ruby/command-t 1>/dev/null
-        rm -f *.o *.so
-        /usr/bin/ruby extconf.rb
-        make
-        cd - 1>/dev/null
-    else
-        rm -rf ~/.vim/bundle/command-t
-    fi
-else
-    rm -rf ~/.vim/bundle/command-t
-fi
-
 # Pyflakes, Flake8 and Jedi only for Python 2.5+
 python -V 2>&1 | grep -q '2\.4'
 if [ $? -eq 0 ];then
